0ebf88655c7b204ad17c567e406eb0280f5abbbd
savotentikutiku-source <savotentikutiku@gmail.com>
2026-02-24 05:14:53 +0900

    Add expense tracking feature

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Schedule;
 use App\Models\Checklist;
+use App\Models\Expense; // 👈 この一行を追加！
 
 class TravelController extends Controller
 {
